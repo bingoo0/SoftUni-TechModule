25d04e7c201ae46c1e44bb43c18dba0416e5d784
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-02 22:47:59 +0200

    Add String Repeater

---
+++ Methods - Exercises/3. String Repeater/StringRepeater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.String_Repeater
+{
+    class StringRepeater
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            var count = int.Parse(Console.ReadLine());
+
+            StringRepeat(name, count);
+
+        }
+
+        static string StringRepeat(string name, int count)
+        {
+            var result = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                result += name;
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}

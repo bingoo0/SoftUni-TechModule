3603b7c827cabc1f9fa372cc24c7532468c4fcc9
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-02 22:40:08 +0200

    Methods - Exercises

---
+++ Methods - Exercises/2. Min Method/MinMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Min_Method
+{
+    class MinMethod
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var thirdNumber = int.Parse(Console.ReadLine());
+
+            GetMin(firstNumber, secondNumber, thirdNumber);
+        }
+
+        static void GetMin(int first, int second, int third)
+        {
+            Console.WriteLine(Math.Min(third, Math.Min(first, second)));
+        }
+    }
+}

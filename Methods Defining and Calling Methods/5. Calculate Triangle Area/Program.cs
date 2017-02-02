1126082e463585bc36cbe4cc1efc7570f87ff3dd
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-02 21:13:47 +0200

    Add Calculate Triangle Area

---
+++ Methods Defining and Calling Methods/5. Calculate Triangle Area/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Calculate_Triangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var weight = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(PrintTriangleArea(weight, height)); 
+        }
+
+        static double PrintTriangleArea(double weight, double height)
+        {
+            return weight * height / 2;
+        }
+    }
+}

04b6ebd54b2252905e68c8a6dbac8fd30ae9697d
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-02 21:55:07 +0200

    Add Math Power and Greater of Two Values

---
+++ Methods Defining and Calling Methods/6. Math Power/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var number = long.Parse(Console.ReadLine());
+            var power = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(MathPower(number, power)); 
+
+        }
+
+        static long MathPower(long number, long power)
+        {
+            var result = 1L;
+
+            for (long i = 0; i < power; i++)
+            {
+                result *= number;   
+            }
+            return result;
+        }
+    }
+}
---
+++ Methods Defining and Calling Methods/7. Greater of Two Values/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Greater_of_Two_Values
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var type = Console.ReadLine();
+            PrintMax(type);
+
+        } // end method
+
+        private static void PrintMax(string type)
+        {
+            switch (type)
+            {
+                case "int":
+                    {
+                        var first = int.Parse(Console.ReadLine());
+                        var second = int.Parse(Console.ReadLine());
+
+                        var greater = GetMax(first, second);
+                        Console.WriteLine(greater);
+
+                        break;
+                    }
+                case "string":
+                    {
+                        var first = Console.ReadLine();
+                        var second = Console.ReadLine();
+
+                        var greater = GetMax(first, second);
+                        Console.WriteLine(greater);
+
+                        break;
+                    }
+                case "char":
+                    {
+                        var first = char.Parse(Console.ReadLine());
+                        var second = char.Parse(Console.ReadLine());
+
+                        var greater = GetMax(first, second);
+                        Console.WriteLine(greater);
+
+                        break;
+                    }
+
+            } // end switch
+        }
+
+        static int GetMax(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            return second;
+        }
+
+        static string GetMax(string first, string second)
+        {
+            if (first.CompareTo(second) >= 0)
+            {
+                return first;
+            }
+            return second;
+        }
+
+        static char GetMax(char first, char second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            return second;
+        }
+    }
+}

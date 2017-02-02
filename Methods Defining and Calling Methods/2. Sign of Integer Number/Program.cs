0fcedda69d6dbfd259478e309d9e4a6393753d1d
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-02 20:54:15 +0200

    Methods: Defining and Calling Methods

---
+++ Methods Defining and Calling Methods/2. Sign of Integer Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Sign_of_Integer_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var number = int.Parse(Console.ReadLine());
+
+            PrintSign(number);
+
+        }
+
+        static int PrintSign(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if(n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+            return n;
+        }
+    }
+}
---
+++ Methods Defining and Calling Methods/3. Printing Triangle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var number = int.Parse(Console.ReadLine());
+
+            PrintTriagle(number);
+        }
+
+        static void PrintTriagle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                PrintLine(i);
+
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                PrintLine(i);
+            }
+            
+            
+        }
+
+        static void PrintLine(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

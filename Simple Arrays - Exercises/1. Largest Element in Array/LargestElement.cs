97ec067bfc86400795274043b04360c8ed5ae71a
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-12 19:30:45 +0200

    Add Largest Element in Array

---
+++ Simple Arrays - Exercises/1. Largest Element in Array/LargestElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.Largest_Element_in_Array
+{
+    class LargestElement
+    {
+        static void Main()
+        {
+            var numbers = int.Parse(Console.ReadLine());
+            var arrayOfNums = new int[numbers];
+
+            var largestNum = int.MinValue;
+            for (int i = 0; i < arrayOfNums.Length; i++)
+            {
+                
+                var currentNumber = int.Parse(Console.ReadLine());
+                arrayOfNums[i] = currentNumber;
+
+                if (currentNumber > largestNum)
+                {
+                    largestNum = currentNumber ;
+                    
+                }   
+            } // end for
+            Console.WriteLine(largestNum);
+        }
+    }
+}

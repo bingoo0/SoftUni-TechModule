51aa048d824290fdb4b533e50cc6397cce9f86b0
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-12 19:37:09 +0200

    Add Count Of Negative Elements In Array

---
+++ Simple Arrays - Exercises/2. Count of Negative Elements in Array/NegativeElementsInArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace _2.Count_of_Negative_Elements_in_Array
+{
+    class NegativeElementsInArray
+    {
+        static void Main()
+        {
+            var numbers = int.Parse(Console.ReadLine());
+
+            var array = new int[numbers];
+            var countOfNegativeNums = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                var currentNum = int.Parse(Console.ReadLine());
+                array[i] = currentNum;
+                if (array[i] < 0)
+                {
+                    countOfNegativeNums++;
+                }
+            }
+            Console.WriteLine(countOfNegativeNums);
+        }
+    }
+}

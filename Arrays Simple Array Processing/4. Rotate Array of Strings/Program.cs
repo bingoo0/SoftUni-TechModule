12e61b6b354ea7ffe709e7326ce736b345be2b6e
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-12 18:03:32 +0200

    Add Rotate Array of Strings

---
+++ Arrays Simple Array Processing/4. Rotate Array of Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Rotate_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split().ToArray();
+
+            var rotatedArray = new string[array.Length];
+            rotatedArray[0] = array[array.Length - 1];
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                  rotatedArray[i + 1] = array[i];
+            }
+
+            
+
+            var result = string.Join(" ", rotatedArray);
+            Console.WriteLine(result);
+        }
+    }
+}

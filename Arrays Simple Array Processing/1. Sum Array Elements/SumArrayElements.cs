7c24499dcebd8dd8ca23c57d399b234aad8cadb6
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-12 11:48:36 +0200

    Simple-Arrays

---
+++ Arrays Simple Array Processing/1. Sum Array Elements/SumArrayElements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace _1.Sum_Array_Elements
+{
+    class SumArrayElements
+    {
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var sumArray = new int[number];
+
+            var sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                var currentNum = int.Parse(Console.ReadLine());
+
+                sumArray[i] = currentNum;
+                sum += sumArray[i];
+            } // end for
+
+            Console.WriteLine(sum);
+        }
+    }
+} // end main 

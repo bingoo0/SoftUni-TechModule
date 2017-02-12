3ee8d68a1078748b62044433d3a214fedf78e271
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-12 17:21:04 +0200

    Add Multiply an Array of Doubles and Smallest Element in Array

---
+++ Arrays Simple Array Processing/2. Multiply an Array of Doubles/MultipleArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _2.Multiply_an_Array_of_Doubles
+{
+    class MultipleArray
+    {
+        static void Main(string[] args)
+        {
+            var number = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            var p = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] *= p;
+            }
+
+            var result = string.Join(" ", number);
+            Console.WriteLine(result);
+        }
+    }
+}

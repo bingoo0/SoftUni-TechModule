15b7382c6f89ace5c2a4f58d27d5e4ac3c71d1f4
bingoo0 <mehmedsezgin01@gmail.com>
2017-02-12 23:18:46 +0200

    Add Array Symmetry

---
+++ Simple Arrays - Exercises/8. Array Symmetry/ArraySymmetry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+namespace _8.Array_Symmetry
+{
+    class ArraySymmetry
+    {
+        static void Main()
+        {
+            var words = Console.ReadLine().Split(' ');
+
+            bool isTrue = true;
+            for (int i = 0; i < words.Length / 2; i++)
+            {
+                var firstWord = words[i];
+                var secondWord = words[words.Length - 1 - i];
+
+                if (firstWord != secondWord)
+                {
+                    isTrue = false;
+                    break;
+                }
+            }
+
+            if (isTrue)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");       
+            }
+        }
+    }
+}

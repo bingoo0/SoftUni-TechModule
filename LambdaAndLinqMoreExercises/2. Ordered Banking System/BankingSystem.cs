2d7eb5b4566b18a7603b7c628958f4fd21dfd861
bingoo0 <mehmedsezgin01@gmail.com>
2017-03-28 21:30:20 +0300

    Add BankingSystem

---
+++ LambdaAndLinqMoreExercises/2. Ordered Banking System/BankingSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.Ordered_Banking_System
+{
+    class BankingSystem
+    {
+        public static void Main()
+        {
+            var bankingSystemDict = new Dictionary<string, Dictionary<string, decimal>>();
+
+            var input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                var splitedInput = input.Split(new[] { " ", "-", ">" }, StringSplitOptions.RemoveEmptyEntries);
+
+                var bank = splitedInput[0];
+                var account = splitedInput[1];
+                var balance = decimal.Parse(splitedInput[2]);
+
+                if (!bankingSystemDict.ContainsKey(bank))
+                {
+                    bankingSystemDict.Add(bank, new Dictionary<string, decimal>());
+                }
+
+                if (!bankingSystemDict[bank].ContainsKey(account))
+                {
+                    bankingSystemDict[bank].Add(account, 0);
+                }
+                bankingSystemDict[bank][account] += balance;
+
+                input = Console.ReadLine();
+            } // end while
+
+            foreach (var bank in bankingSystemDict.OrderByDescending(bank => bank.Value.Sum(account => account.Value)).ThenByDescending(bank => bank.Value.Max(account => account.Value)))
+            {
+                foreach (var accountAndBalance in bank.Value)
+                {
+                    Console.WriteLine($"{accountAndBalance.Key} -> {accountAndBalance.Value} ({bank.Key})");
+                }
+            }
+        }
+    }
+}
---
+++ LambdaAndLinqMoreExercises/LambdaAndLinqMoreExercises/LambdaExpressions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAndLinqMoreExercises
+{
+    class LambdaExpressions
+    {
+       public static void Main()
+        {
+            var dictionary = new Dictionary<string, Dictionary<string, string>>();
+
+            var input = Console.ReadLine();
+
+            while (input != "lambada")
+            {
+                var whileInput = input.Split(new[] { "=", ">", ".", " "}, StringSplitOptions.RemoveEmptyEntries);
+
+                
+
+                if (whileInput.Length > 1)
+                {
+                    var selector = whileInput[0];
+                    var selectorObject = whileInput[1];
+                    var property = whileInput[2];
+
+                    if (!dictionary.ContainsKey(selector))
+                    {
+                        dictionary.Add(selector, new Dictionary<string, string>());
+                    }
+                    dictionary[selector][selectorObject] = property;
+
+                }
+                else
+                {
+                    dictionary = dictionary
+                        .ToDictionary(selector => selector.Key, selector => selector.Value
+                        .ToDictionary(selectorObject => selectorObject.Key, selectorObject => selector.Key + "." + selectorObject.Value));
+                }
+                input = Console.ReadLine();
+            } // end while
+
+            foreach (var selector in dictionary)
+            {
+                foreach (var selectorObject in selector.Value)
+                {
+                    Console.WriteLine($"{selector.Key} => {selectorObject.Key}.{selectorObject.Value}");
+                }
+            }
+        }
+    }
+}
